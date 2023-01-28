eed54f12e40428b19f2d4f6d419044b698670671
Michael Overman <overman.michael@gmail.com>
2023-01-28 11:42:20 -0500

    live data w/observer working simple button counter

--- app/src/main/java/digital/overman/foosballmeet/domain/StandingsViewModel.kt
+++ app/src/main/java/digital/overman/foosballmeet/domain/StandingsViewModel.kt
@@ -1,34 +1,44 @@
 package digital.overman.foosballmeet.domain
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import digital.overman.foosballmeet.data.Match
 import digital.overman.foosballmeet.data.PlayersRepository
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 import kotlin.random.Random
 
 const val TAG = "StandingsViewModel"
-class StandingsViewModel @Inject constructor(private val repository: PlayersRepository) : ViewModel() {
-    val players
-        get() = repository.getPlayersSortedByComparator()
-    val playerList: MutableLiveData<String> by lazy {
-        MutableLiveData<String>()
-    }
+class StandingsViewModel : ViewModel() {
+    var repository = PlayersRepository
+//    private val _players = MutableLiveData("loading...")
+//    val players: LiveData<String>
+//        get() = _players
+
+    private val _dumby = MutableLiveData(0)
+    val dumby: LiveData<Int>
+        get() = _dumby
 //        get() = players.joinToString(separator = " \n")
     init {
-        viewModelScope.launch {
-            repository.playerList.collect { list ->
-                playerList.value = list.joinToString(separator = "\n")
-            }
-        }
+//        getPlayerList()
     }
 
+    fun getPlayerList() {
+//        viewModelScope.launch {
+//            _players.value = repository.playerList.first().joinToString("\n")
+//            repository.playerList.collect { list ->
+//                _players.value = list.joinToString(separator = "\n")
+//            }
+//        }
+    }
     fun addMatchClicked() {
         val match = Match("Ada", Random.nextInt(0,8), "Dad", Random.nextInt(2,10))
         repository.addMatch(match)
+        Log.d(TAG, "Repository has ${repository.matches.size} matches")
+//        _players.value = repository.getPlayersSortedByComparator().joinToString(separator = "\n")
+        _dumby.value = _dumby.value?.inc()
+        Log.d(TAG, "_Dumby: ${_dumby.value.toString()}")
+        Log.d(TAG, "Dumby: ${dumby.value.toString()}")
     }
 
 }
--- app/src/main/java/digital/overman/foosballmeet/ui/standings/StandingsFragment.kt
+++ app/src/main/java/digital/overman/foosballmeet/ui/standings/StandingsFragment.kt
@@ -6,33 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import digital.overman.foosballmeet.R
 import digital.overman.foosballmeet.databinding.StandingsFragmentBinding
 import digital.overman.foosballmeet.domain.StandingsViewModel
-import javax.inject.Inject
 
-@AndroidEntryPoint
 class StandingsFragment : Fragment() {
 
-    @Inject lateinit var viewModel: StandingsViewModel
+    private val viewModel: StandingsViewModel by viewModels()
     private lateinit var binding: StandingsFragmentBinding
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.standings_fragment, container, false)
+//        val playerListObserver = Observer<String> { newList ->
+//
+//            binding.playerlistTv.text = newList
+//        }
+        val dumbyObserver = Observer<Int> { newInt ->
+            binding.dumbyTv.text = newInt.toString()
+        }
+
+        viewModel.dumby.observe(viewLifecycleOwner, dumbyObserver)
+
+        binding.fab.setOnClickListener { viewModel.addMatchClicked() }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.standingsViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
     }
 }
